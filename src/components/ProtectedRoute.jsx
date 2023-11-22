5b6717075cdd008af5357e614099647f57322a7d
Thomas J <thomas.jallu@gmail.com>
2023-11-22 01:38:19 +0100

    New components : - Folder of BackOffice components   - Sidebar   - BackOfficeLayout   - BackOfficeNavbar - ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,14 @@
 import {Navigate} from "react-router-dom";
+import {isArray} from "lodash";
 
 // eslint-disable-next-line react/prop-types,no-unused-vars
-export default function ProtectedRoute({ children, role }) {
+export default function ProtectedRoute({ children, roleAllowed }) {
     const isConnected = true; // Todo faire hook pour vérifier si l'utilisateur est connecté
 
     // Todo faire en sorte de récupérer le rôle de l'user via son token (jwt) et s'il match avec le rôle passé en props alors c'est good
     // Todo donc il faut aussi faire un hook pour récupérer le token de l'utilisateur et le décoder pour récupérer les données de l'user
-    const isAuthorized = role === "manager";
+    // eslint-disable-next-line react/prop-types
+    const isAuthorized = isArray(roleAllowed) && roleAllowed?.includes("manager");
 
     if(!isConnected) {
         return <Navigate to="/login" />;
